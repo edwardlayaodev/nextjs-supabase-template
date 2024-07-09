b824ab96da4daae4558b61c03728c31e6d619994
Edward Layao <edward.layao.dev@proton.me>
2024-07-09 13:43:05 +0800

    add atom components

--- app/_components/atoms/Button.tsx
+++ app/_components/atoms/Button.tsx
@@ -1,7 +1,7 @@
 interface Props {
   children: React.ReactNode;
   buttonType: "btn-primary" | "btn-secondary" | "btn-neutral" | "btn-ghost";
-  onClick?: MouseEvent;
+  onClick?: Function;
 }
 
 export default function Button({ children, buttonType }: Props) {
