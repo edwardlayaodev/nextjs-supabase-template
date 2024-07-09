b4eeb9e30ee435386b8427f69a9785450ee5101c
Edward Layao <edward.layao.dev@proton.me>
2024-07-09 13:45:39 +0800

    add routes for signup page

---
+++ app/_utils/validators.ts
@@ -0,0 +1,10 @@
+// validators.js
+
+export function validateEmail(email: string) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+export function validatePassword(password: string) {
+  return password && password.length >= 8;
+}
