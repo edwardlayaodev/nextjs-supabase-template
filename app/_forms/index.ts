ecf83467ad06bf59ce920ad6b532acc6bad6d70f
Edward Layao <edward.layao.dev@proton.me>
2024-07-29 16:34:15 +0800

    remove testinput in entrypoint

--- app/_forms/index.ts
+++ app/_forms/index.ts
@@ -1,15 +1,12 @@
 import SignIn from "./SignIn/SignIn";
 import SignUp from "./SignUp/SignUp";
-import TestInput from "./TestInput";
 
 /**
  * Exported Form object containing various form components.
  * @property {React.ComponentType} SignIn - Component for sign-in form.
  * @property {React.ComponentType} SignUp - Component for sign-up form.
- * @property {React.ComponentType} TestInput - Component for test input form.
  */
 export const Form = {
   SignIn,
   SignUp,
-  TestInput,
 };
