06bbf2b1a66d5ab003af1c1ae36ec65e6c295262
Edward Layao <edward.layao.dev@proton.me>
2024-07-29 14:32:58 +0800

    add comments on Animated Atom

--- app/_components/atoms/Animated.tsx
+++ app/_components/atoms/Animated.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { AnimatePresence, motion } from "framer-motion";
-import { Atom } from ".";
 
+/**
+ * Interface for Animated properties.
+ * @param {children} React.ReactNode - The Child Element, content of the animated container.
+ * @param {isVisible} boolean - The state in which the children should be rendered, this triggers animate presence when false, which makes the animation finish first before unmounting.
+ * @param {extraClass} string - Extra classes for the animated container.
+ */
 interface Props {
   children: React.ReactNode;
   isVisible: boolean;
   extraClass: string;
 }
 
+/**
+ * Animated component for handling animations onMount and onDestroy.
+ * @param {Props} props - The props for the Animated component.
+ * @returns
+ */
 export default function Animated({ children, isVisible, extraClass }: Props) {
   return (
     <AnimatePresence>
