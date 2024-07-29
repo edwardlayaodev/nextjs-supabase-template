55001d902f29756ec13fa10f97e0b13c86a16b47
Edward Layao <edward.layao.dev@proton.me>
2024-07-29 16:07:33 +0800

    add animations for forms

--- app/_forms/SignIn/InnerForm.tsx
+++ app/_forms/SignIn/InnerForm.tsx
@@ -1,6 +1,8 @@
 import { Atom } from "@/app/_components/atoms";
+import { Molecule } from "@/app/_components/molecules";
 import SignInFormValues from "@/app/types/SignInFormValues";
 import { FormikProps } from "formik";
+import { useEffect, useState } from "react";
 
 /**
  * InnerForm component for rendering the form UI.
@@ -8,6 +10,7 @@ import { FormikProps } from "formik";
  * @returns JSX.Element
  */
 export const InnerForm = (props: FormikProps<SignInFormValues>) => {
+  const [isOpen, setIsOpen] = useState(false);
   const inputFields = [
     { type: "email", name: "email", label: "Email" },
     { type: "password", name: "password", label: "Password" },
@@ -22,25 +25,41 @@ export const InnerForm = (props: FormikProps<SignInFormValues>) => {
         onBlurHandler={props.handleBlur}
         touched={props.touched}
         errors={props.errors}
-        value={props.values.email}
+        value={props.values[item.name as "email" | "password"]}
         name={item.name}
         type={item.type as "email" | "password"}
       />
     );
   });
 
-  return (
-    <section className="flex flex-col gap-4">
-      <h1 className="mx-auto text-2xl font-bold">Sign-in</h1>
-      <form
-        className="flex flex-col justify-center items-center gap-4"
-        onSubmit={props.handleSubmit}
-      >
-        {renderInputFields}
+  useEffect(() => {
+    setIsOpen(props.status ? true : false);
+  }, [props.status]);
 
-        <Atom.Button buttonType={"btn-primary"}>Submit</Atom.Button>
-        <div>{props.status}</div>
-      </form>
-    </section>
+  function handleClose() {
+    setIsOpen(false);
+    props.setStatus(undefined);
+  }
+
+  return (
+    <>
+      <Molecule.Alert
+        isOpen={isOpen}
+        type={"error"}
+        message={props.status}
+        onClose={handleClose}
+      ></Molecule.Alert>
+      <section className="flex flex-col gap-4">
+        <h1 className="mx-auto text-2xl font-bold">Sign-in</h1>
+        <form
+          className="flex flex-col justify-center items-center gap-4"
+          onSubmit={props.handleSubmit}
+        >
+          {renderInputFields}
+
+          <Atom.Button buttonType={"btn-primary"}>Submit</Atom.Button>
+        </form>
+      </section>
+    </>
   );
 };
